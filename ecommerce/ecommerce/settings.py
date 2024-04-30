ce0be96de07032bd70fd41508b58a9275b4acd8d
joao.reberth <joaoreberthsant@gmail.com>
2024-04-30 14:29:44 -0300

    Create models, create cart, create chekout cart, end functions

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -115,6 +115,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/products')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
